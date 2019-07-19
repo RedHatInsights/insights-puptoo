64284843042664fd8caaddf97798f7f63978d1e4
Stephen Adams <tsadams@gmail.com>
2019-07-19 11:38:42 -0400

    Serialize item as JSON

--- producer.py
+++ producer.py
@@ -1,7 +1,9 @@
 import config
+import json
 
 from kafka import KafkaProducer
 
 def init_producer():
-    producer = KafkaProducer(bootstrap_servers=config.BOOTSTRAP_SERVERS)
+    producer = KafkaProducer(bootstrap_servers=config.BOOTSTRAP_SERVERS,
+                             value_serializer=lambda x: json.dumps(x).encode("utf-8"))
     return producer
