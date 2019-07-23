fb0d8fd7ea81cdfdce2d1f3e744c11db9818e9b2
Stephen Adams <tsadams@gmail.com>
2019-07-23 11:16:10 -0400

    Add service key to tracker
    Signed-off-by: Stephen Adams <tsadams@gmail.com>

--- tracker.py
+++ tracker.py
@@ -9,6 +9,7 @@ def tracker_msg(extra, status, status_msg):
     message = {"account": extra["account"],
                "request_id": extra["request_id"],
                "payload_id": extra["request_id"],
+               "service": "puptoo",
                "status": status,
                "status_msg": status_msg,
                "date": get_time()
