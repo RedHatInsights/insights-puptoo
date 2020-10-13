c087a669432756bdad57b48cdda213c803773342
Kyle Lape <klape@redhat.com>
2020-10-13 14:11:02 -0500

    Log in to registry.redhat.io

--- build_deploy.sh
+++ build_deploy.sh
@@ -13,5 +13,6 @@ fi
 DOCKER_CONF="$PWD/.docker"
 mkdir -p "$DOCKER_CONF"
 docker --config="$DOCKER_CONF" login -u="$QUAY_USER" -p="$QUAY_TOKEN" quay.io
+docker --config="$DOCKER_CONF" login -u="$RH_REGISTRY_USER" -p="$RH_REGISTRY_TOKEN" registry.redhat.io
 docker --config="$DOCKER_CONF" build -t "${IMAGE}:${IMAGE_TAG}" .
 docker --config="$DOCKER_CONF" push "${IMAGE}:${IMAGE_TAG}"
