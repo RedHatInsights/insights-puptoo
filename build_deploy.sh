991e261c03132f60458da2712cf9fc126b785930
Kyle Lape <klape@redhat.com>
2020-10-22 08:58:37 -0500

    Also push QA tag

--- build_deploy.sh
+++ build_deploy.sh
@@ -21,4 +21,6 @@ docker --config="$DOCKER_CONF" push "${IMAGE}:${IMAGE_TAG}"
 if [ "${PUSH_TO_LATEST:=true}" == "true" ]; then
     docker --config="$DOCKER_CONF" tag "${IMAGE}:${IMAGE_TAG}" "${IMAGE}:${SMOKE_TEST_TAG}"
     docker --config="$DOCKER_CONF" push "${IMAGE}:${SMOKE_TEST_TAG}"
+    docker --config="$DOCKER_CONF" tag "${IMAGE}:${IMAGE_TAG}" "${IMAGE}:qa"
+    docker --config="$DOCKER_CONF" push "${IMAGE}:qa"
 fi
