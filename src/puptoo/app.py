e70c3b97027f5a549be1326675cb4a6898febf96
Stephen Adams <tsadams@gmail.com>
2021-03-09 08:51:42 -0500

    task: adjust staletime to 29 hours
    Since there is 4 hour drift on insights-client uploads. It's possible to
    miss the window and have a system appear as stale. Increasing our
    staletime to 29 hours should help avoid this.
    
    RHCLOUD-12827
    
    Signed-off-by: Stephen Adams <tsadams@gmail.com>

--- src/puptoo/app.py
+++ src/puptoo/app.py
@@ -34,7 +34,7 @@ def get_extra(account="unknown", request_id="unknown"):
 
 
 def get_staletime():
-    the_time = datetime.now() + timedelta(hours=26)
+    the_time = datetime.now() + timedelta(hours=29)
     return the_time.astimezone().isoformat()
 
 
