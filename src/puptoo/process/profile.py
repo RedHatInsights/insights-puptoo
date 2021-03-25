9890d73bec9ec6d3976126b669a69643545b5b1a
mhornick <mhornick@redhat.com>
2021-03-25 16:04:35 +0100

    Use empty string for systems with no releasever
    Signed-off-by: mhornick <mhornick@redhat.com>

--- src/puptoo/process/profile.py
+++ src/puptoo/process/profile.py
@@ -297,8 +297,8 @@ def system_profile(
             catch_error("redhat_release", e)
             raise
 
-    # By default, always provide None value
-    profile["rhsm"] = None
+    # When inventory allows us to delete system facts, do that instead using empty string here
+    profile["rhsm"] = {"version": ""}
     if rhsm_releasever:
         try:
             # We can add pre-parsed minor + major values, but the schema specifies just version
