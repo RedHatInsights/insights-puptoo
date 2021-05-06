ad6e083a19ca481d9a2ac9c5fe68e25ebe43a0c0
Brandon Squizzato <bsquizza@redhat.com>
2021-05-06 16:31:17 -0400

    Make unit tests run first

--- pr_check.sh
+++ pr_check.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Run unit tests first since it activates/deactives its own virtual env
+source unit_test.sh
+
 # --------------------------------------------
 # Options that must be configured by app owner
 # --------------------------------------------
@@ -11,12 +14,10 @@ IQE_PLUGINS="ingress"
 IQE_MARKER_EXPRESSION="smoke"
 IQE_FILTER_EXPRESSION=""
 
-
 # Install bonfire repo/initialize
 CICD_URL=https://raw.githubusercontent.com/RedHatInsights/bonfire/master/cicd
 curl -s $CICD_URL/bootstrap.sh > .cicd_bootstrap.sh && source .cicd_bootstrap.sh
 
 source $CICD_ROOT/build.sh
-source $APP_ROOT/unit_test.sh
 source $CICD_ROOT/deploy_ephemeral_env.sh
 source $CICD_ROOT/smoke_test.sh
