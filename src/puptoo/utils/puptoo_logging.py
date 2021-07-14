ad75f4fb960eb83f51e85f6c7de60120a8449ab1
Stephen Adams <tsadams@gmail.com>
2021-07-14 14:55:46 -0400

    [cloudwatch] re-add cloudwatch logging
    We removed haberdasher due to some performance issues. putting back in
    direct logs to cloudwatch
    
    Signed-off-by: Stephen Adams <tsadams@gmail.com>

--- src/puptoo/utils/puptoo_logging.py
+++ src/puptoo/utils/puptoo_logging.py
@@ -1,7 +1,9 @@
 import os
 import sys
 import logging
+import socket
 from threading import local
+from logstash_formatter import LogstashFormatterV1
 
 from . import config
 
@@ -9,11 +11,34 @@ from . import config
 threadctx = local()
 
 
+def clowder_config():
+    # Cloudwatch Configuration with Clowder
+    if os.environ.get("ACG_CONFIG"):
+        import app_common_python
+
+        cfg = app_common_python.LoadedConfig
+        if cfg.logging:
+            cw = cfg.logging.cloudwatch
+            return cw.accessKeyId, cw.secretAccessKey, cw.region, cw.logGroup, False
+        else:
+            return None, None, None, None, None
+
+
+def non_clowder_config():
+    aws_access_key_id = os.getenv("AWS_ACCESS_KEY_ID", None)
+    aws_secret_access_key = os.getenv("AWS_SECRET_ACCESS_KEY", None)
+    aws_region_name = os.getenv("AWS_REGION_NAME", None)
+    aws_log_group = os.getenv("AWS_LOG_GROUP", "platform")
+    create_log_group = str(os.getenv("AWS_CREATE_LOG_GROUP")).lower() == "true"
+    return aws_access_key_id, aws_secret_access_key, aws_region_name, aws_log_group, create_log_group
+
+
 def initialize_logging():
     kafkalogger = logging.getLogger("kafka")
     kafkalogger.setLevel(config.KAFKA_LOGGER)
     if any("KUBERNETES" in k for k in os.environ):
-        handler = logging.StreamHandler(sys.stderr)
+        handler = logging.StreamHandler(sys.stdout)
+        handler.setFormatter(LogstashFormatterV1())
         handler.addFilter(ContextualFilter())
         logging.root.setLevel(os.getenv("LOG_LEVEL", "INFO"))
         logging.root.addHandler(handler)
@@ -22,6 +47,31 @@ def initialize_logging():
             level=config.LOG_LEVEL,
             format="%(threadName)s %(levelname)s %(name)s - %(message)s",
         )
+
+    if os.environ.get("ACG_CONFIG"):
+        f = clowder_config
+    else:
+        f = non_clowder_config
+
+    aws_access_key_id, aws_secret_access_key, aws_region_name, aws_log_group, create_log_group = f()
+
+    if all((aws_access_key_id, aws_secret_access_key, aws_region_name, aws_log_group)):
+        from boto3.session import Session
+        import watchtower
+
+        boto3_session = Session(aws_access_key_id=aws_access_key_id,
+                                aws_secret_access_key=aws_secret_access_key,
+                                region_name=aws_region_name)
+
+        # configure logging to use watchtower
+        cw_handler = watchtower.CloudWatchLogHandler(boto3_session=boto3_session,
+                                                  log_group=aws_log_group,
+                                                  stream_name=socket.gethostname(),
+                                                  create_log_group=create_log_group)
+
+        cw_handler.setFormatter(LogstashFormatterV1())
+        cw_handler.addFilter(ContextualFilter())
+        logging.root.addHandler(cw_handler)
 
     logger = logging.getLogger(config.APP_NAME)
 
