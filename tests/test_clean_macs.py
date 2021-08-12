3c40d412495a3fc8b0afbbbf0084e33eab69782f
Stephen Adams <tsadams@gmail.com>
2021-08-12 10:38:51 -0400

    [fix] filter bad macs from canonical facts
    Match mac addresses against regex to prevent bad macs from making it
    into inventory via canonical facts
    
    Signed-off-by: Stephen Adams <tsadams@gmail.com>

--- tests/test_clean_macs.py
+++ tests/test_clean_macs.py
@@ -7,7 +7,8 @@ GOOD_MACS = {"metadata": {"mac_addresses": ["52:54:00:fd:be:d0",
 BAD_MACS = {"metadata": {"mac_addresses": ["0.0.0.0",
             "52:54:00:fd:be:d0",
              "b4:6b:fc:9e:ae:0d",
-             "0.0.0.0"]}}
+             "0.0.0.0",
+             "00:00:00:00"]}}
 
 
 def test_clean_macs():
