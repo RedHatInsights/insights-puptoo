17e91dd99a8386daed30c06c3eb9ab10383359d3
codeheeler <dawalker@redhat.com>
2021-11-19 13:28:38 -0500

    Add mssql_version fact to system_profile

---
+++ tests/test_mssql_info.py
@@ -0,0 +1,14 @@
+from insights.specs import Specs
+from insights.tests import InputData, run_test
+
+from src.puptoo.process.profile import system_profile
+
+RPMS = """
+mssql-server-1.0.4-1.x86_64  Tue 14 Jul 2015 09:25:38 AEST   1398536494
+""".strip()
+
+
+def test_mssql_info():
+    input_data = InputData().add(Specs.installed_rpms, RPMS)
+    result = run_test(system_profile, input_data)
+    assert result["mssql_version"] == "1.0.4"
