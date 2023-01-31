59a7ba069c42801d76396000758ba8afb9356ac5
Stephen Adams <tsadams@gmail.com>
2023-01-31 16:07:59 -0500

    Refactor SSL for Kafka
    With the newest environment we need to decouple the security.protocol
    value for kafka from the sasl configuration. In fedramp, we need to have
    this available if we have a ca cert. Not just when we have a username
    for authentication.
    
    Signed-off-by: Stephen Adams <tsadams@gmail.com>

--- src/puptoo/mq/produce.py
+++ src/puptoo/mq/produce.py
@@ -4,24 +4,7 @@ from ..utils import config
 
 
 def init_producer():
-    connection_info = {}
-    if config.KAFKA_BROKER:
-        connection_info[
-            "bootstrap.servers"
-        ] = f"{config.KAFKA_BROKER.hostname}:{config.KAFKA_BROKER.port}"
-        if config.KAFKA_BROKER.cacert:
-            connection_info["ssl.ca.location"] = "/tmp/cacert"
-        if config.KAFKA_BROKER.sasl and config.KAFKA_BROKER.sasl.username:
-            connection_info.update(
-                {
-                    "security.protocol":config.KAFKA_BROKER.sasl.securityProtocol,
-                    "sasl.mechanisms": config.KAFKA_BROKER.sasl.saslMechanism,
-                    "sasl.username": config.KAFKA_BROKER.sasl.username,
-                    "sasl.password": config.KAFKA_BROKER.sasl.password,
-                }
-            )
-    else:
-        connection_info["bootstrap.servers"] = ",".join(config.BOOTSTRAP_SERVERS)
+    connection_info = config.kafka_config()
 
     producer = Producer(connection_info)
     return producer
