7a0364e22c4d31bdc185662092a99a9d747c1702
Victor M <vmugicag@redhat.com>
2023-02-03 13:44:23 +0100

    delete-this-commit

--- pr_check.sh
+++ pr_check.sh
@@ -9,6 +9,8 @@ source unit_test.sh
 APP_NAME="ingress"  # name of app-sre "application" folder this component lives in
 COMPONENT_NAME="puptoo"  # name of app-sre "resourceTemplate" in deploy.yaml for this component
 IMAGE="quay.io/cloudservices/insights-puptoo"
+BONFIRE_REPO_ORG="Victoremepunto"
+BONFIRE_REPO_BRANCH="set-expiry-label-as-parameter"
 
 IQE_PLUGINS="e2e"
 IQE_MARKER_EXPRESSION="smoke"
@@ -16,10 +18,10 @@ IQE_FILTER_EXPRESSION=""
 IQE_CJI_TIMEOUT="30m"
 
 # Install bonfire repo/initialize
-CICD_URL=https://raw.githubusercontent.com/RedHatInsights/bonfire/master/cicd
+CICD_URL=https://raw.githubusercontent.com/Victoremepunto/bonfire/master/cicd
 curl -s $CICD_URL/bootstrap.sh > .cicd_bootstrap.sh && source .cicd_bootstrap.sh
 
 source $CICD_ROOT/build.sh
-source $CICD_ROOT/deploy_ephemeral_env.sh
+#source $CICD_ROOT/deploy_ephemeral_env.sh
 
-source $CICD_ROOT/cji_smoke_test.sh
+#source $CICD_ROOT/cji_smoke_test.sh
