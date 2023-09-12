787c708eb2a9ba9941f37423706bfb8db5f920f0
Rohini Chandra <61837065+r14chandra@users.noreply.github.com>
2023-09-12 12:02:30 +0530

    update insights-core version to 3.2.15 and add tests for aws ip address (#296)

--- tests/test_aws_public_ipv4_addresses.py
+++ tests/test_aws_public_ipv4_addresses.py
@@ -6,9 +6,42 @@ AWSPUBLICIPV4ADDRESSES = """
 1.2.3.4
 """.strip()
 
+AWS_CURL_ERROR_1 = """
+<?xml version="1.0" encoding="iso-8859-1"?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+        "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+ <head>
+  <title>404 - Not Found</title>
+ </head>
+ <body>
+  <h1>404 - Not Found</h1>
+ </body>
+</html>
+"""
+
+AWS_CURL_ERROR_2 = """
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+        "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+ <head>
+  <title>404 - Not Found</title>
+ </head>
+ <body>
+  <h1>404 - Not Found</h1>
+ </body>
+</html>
+"""
+
 def test_aws_public_ipv4_addresses():
     input_data = InputData().add(Specs.aws_public_ipv4_addresses, AWSPUBLICIPV4ADDRESSES)
     result = run_test(system_profile, input_data)
     assert result["public_ipv4_addresses"] == ["1.2.3.4"]
 
+    input_data = InputData().add(Specs.aws_public_ipv4_addresses, AWS_CURL_ERROR_1)
+    result = run_test(system_profile, input_data)
+    assert result.get("public_ipv4_addresses") == None
 
+    input_data = InputData().add(Specs.aws_public_ipv4_addresses, AWS_CURL_ERROR_2)
+    result = run_test(system_profile, input_data)
+    assert result.get("public_ipv4_addresses") == None
