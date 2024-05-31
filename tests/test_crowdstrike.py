64177a2ca2471cbb4e1fde8fb1bb5fbd22ded966
Xiaoxue Wang <xiaoxwan@redhat.com>
2024-05-31 10:55:29 +0800

    RHINENG-9772: add "crowdstrike" field to system_profile
    Signed-off-by: Xiaoxue Wang <xiaoxwan@redhat.com>

---
+++ tests/test_crowdstrike.py
@@ -0,0 +1,44 @@
+from insights.specs import Specs
+from insights.tests import InputData, run_test
+from src.puptoo.process.profile import system_profile
+
+FALCONCTL_AID = """
+aid="44e3b7d20b434a2bb2815d9808fa3a8b".
+""".strip()
+
+FALCONCTL_BACKEND = """
+backend=kernel.
+""".strip()
+
+
+def test_crowdstrike():
+    input_data = InputData()
+    input_data.add(Specs.falconctl_aid, FALCONCTL_AID)
+    input_data.add(Specs.falconctl_backend, FALCONCTL_BACKEND)
+    result = run_test(system_profile, input_data)
+    assert result["crowdstrike"] == {
+        "falcon_aid": "44e3b7d20b434a2bb2815d9808fa3a8b",
+        "falcon_backend": "kernel",
+    }
+
+    input_data = InputData()
+    input_data.add(Specs.falconctl_aid, FALCONCTL_AID)
+    input_data.add(Specs.falconctl_backend, "")
+    result = run_test(system_profile, input_data)
+    assert result["crowdstrike"] == {
+        "falcon_aid": "44e3b7d20b434a2bb2815d9808fa3a8b",
+    }
+
+    input_data = InputData()
+    input_data.add(Specs.falconctl_aid, "")
+    input_data.add(Specs.falconctl_backend, FALCONCTL_BACKEND)
+    result = run_test(system_profile, input_data)
+    assert result["crowdstrike"] == {
+        "falcon_backend": "kernel",
+    }
+
+    input_data = InputData()
+    input_data.add(Specs.falconctl_aid, "")
+    input_data.add(Specs.falconctl_backend, "")
+    result = run_test(system_profile, input_data)
+    assert "crowdstrike" not in result
