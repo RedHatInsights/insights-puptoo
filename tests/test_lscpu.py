91aa28fb87c11644a8048f522c250b320ac93011
Xiaoxue Wang <xiaoxwan@redhat.com>
2025-02-11 15:56:03 +0800

    RHINENG-2607: add "threads_per_core" field to sp (#418)
    Signed-off-by: Xiaoxue Wang <xiaoxwan@redhat.com>

--- tests/test_lscpu.py
+++ tests/test_lscpu.py
@@ -49,12 +49,43 @@ NUMA node0 CPU(s):   0-3
 Flags:               fp asimd evtstrm aes pmull sha1 sha2 crc32 atomics fphp asimdhp cpuid asimdrdm lrcpc dcpop asimddp ssbs
 """.strip()
 
+LSCPU_3 = """
+Architecture:          x86_64
+CPU op-mode(s):        32-bit, 64-bit
+Byte Order:            Little Endian
+CPU(s):                2
+On-line CPU(s) list:   0,1
+Core(s) per socket:    1
+Socket(s):             1
+NUMA node(s):          1
+Vendor ID:             GenuineIntel
+CPU family:            6
+Model:                 60
+Model name:            Intel Core Processor (Haswell, no TSX)
+Stepping:              1
+CPU MHz:               2793.530
+BogoMIPS:              5587.06
+Hypervisor vendor:     KVM
+Virtualization type:   full
+L1d cache:             32K
+L1i cache:             32K
+L2 cache:              4096K
+NUMA node0 CPU(s):     0,1
+""".strip()
+
 
 def test_lscpu():
     input_data = InputData().add(Specs.lscpu, LSCPU_1)
     result = run_test(system_profile, input_data)
     assert result["cores_per_socket"] == 1
+    assert result["threads_per_core"] == 2
 
     input_data = InputData().add(Specs.lscpu, LSCPU_2)
     result = run_test(system_profile, input_data)
     assert result.get("cores_per_socket") is None
+    assert result["threads_per_core"] == 1
+
+    input_data = InputData().add(Specs.lscpu, LSCPU_3)
+    result = run_test(system_profile, input_data)
+    assert result["cores_per_socket"] == 1
+    assert result.get("threads_per_core") is None
