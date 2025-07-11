b54c1ce36bd1b25061dce4e2c6d488c7e1002ef7
Chris Hambridge <chambrid@redhat.com>
2025-07-11 08:59:21 -0400

    RHINENG-18719: Update Konflux for hermetic and source builds (#474)

--- dev/parse_json.py
+++ dev/parse_json.py
@@ -2,11 +2,14 @@
 # Utility for parsing JSON for tests
 
 import json
+import os
 
+# Get the file path from OUTPUT_JSON environment variable, default to "output.json"
+json_file_path = os.getenv("OUTPUT_JSON", "output.json")
 
-with open("output.json", "r") as f:
+with open(json_file_path, "r") as f:
     data = json.loads(f.read())
     data = data["system"]["metadata"]
 
-with open("output.json", "w") as f:
+with open(json_file_path, "w") as f:
     f.write(json.dumps(data))
