df51314896d78bf2d637c1f81160c86e16db9b23
Xiaoxue Wang <xiaoxwan@redhat.com>
2025-10-30 11:27:35 +0800

    RHINENG-20859: pull system-profile schemas from HBI repo (#532)
      Upon the new process for requesting system profile fields, the system
      profile schemas source had been moved from inventory-schemas repo to
      HBI repo. Adapt the changes here.
    
    Signed-off-by: Xiaoxue Wang <xiaoxwan@redhat.com>

--- unit_test.sh
+++ unit_test.sh
@@ -9,15 +9,18 @@ pip install --upgrade pip
 pip install .
 pip install -r requirements.txt
 pip install -r requirements-dev.txt
+
+echo "### Running unit test ..."
 INSIGHTS_FILTERS_ENABLED=false INVENTORY_TOPIC=platform.inventory.host-ingress-p1 ACG_CONFIG=dev/cdappconfig.json pytest tests/ --junitxml=$WORKSPACE/artifacts/junit-unit_tests.xml
 
 if [ $? != 0 ]; then
-   echo "Failed to run unit test"
+   echo ">>> unit test result: FAILURE"
    exit 1
 fi
+echo ">>> unit test result: SUCCESS"
 
-# Clone inventory-schemas to /tmp
-git clone https://github.com/RedHatInsights/inventory-schemas.git /tmp/inventory-schemas
+# Clone system-profile schema repo to /tmp
+git clone https://github.com/RedHatInsights/insights-host-inventory.git /tmp/insights-host-inventory
 
 echo '---------------------------------'
 echo ' Run the profile and schema check'
@@ -27,24 +30,30 @@ for file in dev/test-archives/*; do
     # Create tar archive in /tmp
     tar -zcf /tmp/$filename "$file"
     # Run insights-run and output to /tmp/output.json
+    echo "### Running insights-run to populate profile on $file ..."
     insights-run -p src/puptoo -f json /tmp/$filename > /tmp/output.json
     insights-run -p src/puptoo /tmp/$filename # Print results in test console
     if [ $? != 0 ]; then
+        echo ">>> Profile result: FAILURE"
         exit 1
     fi
+    echo ">>> Profile result: SUCCESS"
     OUTPUT_JSON="/tmp/output.json" python3.11 dev/parse_json.py
     # Run schema tester with files in /tmp
-    python3.11 /tmp/inventory-schemas/tools/simple-test/tester.py /tmp/inventory-schemas/schemas/system_profile/v1.yaml /tmp/output.json
+    echo "### Checking output against schema insights-host-inventory/swagger/system_profile.spec.yaml ..."
+    python3.11 /tmp/insights-host-inventory/tools/simple-test/tester.py /tmp/insights-host-inventory/swagger/system_profile.spec.yaml /tmp/output.json
     if [ $? != 0 ]; then
+        echo ">>> Schema validation result: FAILURE"
         exit 1
     fi
+    echo ">>> Schema validation result: SUCCESS"
     # Clean up files
     rm /tmp/$filename
     rm /tmp/output.json
 done
 
-# Clean up inventory-schemas directory
-rm -rf /tmp/inventory-schemas
+# Clean up schema repo directory
+rm -rf /tmp/insights-host-inventory
 
 # Deactivate virtual environment
 deactivate
