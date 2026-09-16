c65879b091b689fca4fffcdae523d1f4114bdf96
Pranav Lawate <pran.lawate@gmail.com>
2026-09-16 16:47:59 +0530

    feat(RHINENG-27922 2.9): wire QPCHandler exception->metric mapping (#935)

--- src/puptoo/handlers/qpc.py
+++ src/puptoo/handlers/qpc.py
@@ -1,9 +1,12 @@
+import json
 import logging
 
 from . import handler
 from .base import BaseHandler
+from ..exceptions import FailExtractException, QPCKafkaMsgException
 from ..qpc.report_processor import process_report
 from ..qpc.validators import validate_qpc_message
+from ..utils import metrics
 
 logger = logging.getLogger(__name__)
 
@@ -11,5 +14,17 @@ logger = logging.getLogger(__name__)
 @handler("qpc")
 class QPCHandler(BaseHandler):
     def handle(self, msg: dict, service: str, extra: dict, *, send_message) -> None:
-        request_obj = validate_qpc_message(msg)
-        process_report(msg, request_obj)
+        try:
+            request_obj = validate_qpc_message(msg)
+            process_report(msg, request_obj)
+        except json.JSONDecodeError:
+            logger.exception("QPC message is not valid JSON")
+        except QPCKafkaMsgException:
+            metrics.qpc_kafka_failures.inc()
+            logger.exception("Invalid QPC Kafka message")
+        except FailExtractException:
+            metrics.qpc_extract_report_slices_failures.inc()
+            logger.exception("Failed to extract QPC report")
+        except Exception:
+            metrics.qpc_report_processing_exceptions.inc()
+            logger.exception("Unexpected error processing QPC message")
